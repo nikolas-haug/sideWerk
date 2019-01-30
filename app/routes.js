4b361c01433c06111832d16fb8321384f958905f
Nikolas  Haug <nikohaug21@gmail.com>
2019-01-30 17:05:39 -0600

    add logged in logic to user views

--- app/routes.js
+++ app/routes.js
@@ -14,8 +14,14 @@ module.exports = function(app, passport) {
 	// show the login form
 	app.get('/login', function(req, res) {
 
-		// render the page and pass in any flash data if it exists
-		res.render('login', { message: req.flash('loginMessage') });
+		// CHECK IF USER EXISTS BEFORE RENDERING PAGE
+		if(!req.user) {
+			// render the page and pass in any flash data if it exists
+			res.render('login', { message: req.flash('loginMessage') });
+		} else {
+			res.render('home');
+		}
+		
 	});
 
 	// process the login form
@@ -40,8 +46,13 @@ module.exports = function(app, passport) {
 	// =====================================
 	// show the signup form
 	app.get('/signup', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('signup', { message: req.flash('signupMessage') });
+		// CHECK IF USER EXISTS BEFORE RENDERING PAGE
+		if(!req.user) {
+			// render the page and pass in any flash data if it exists
+			res.render('signup', { message: req.flash('signupMessage') });
+		} else {
+			res.render('home');
+		}
 	});
 
 	// process the signup form
