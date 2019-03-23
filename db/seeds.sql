88feb4e016e9714cb22223b5097d07f5f8efeb21
annaaxell <we6866gc@go.minneapolis.edu>
2019-03-23 16:13:50 -0500

    added styles to master

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO items(item_name)
-VALUES('roll silverware'), ('fold linens'), ('hokie floors'), ('polish cups');
+VALUES('roll silverware'), ('fold linens'), ('hokie floors'), ('polish cups')('Sweep'), ('Fill Jams'), ('Fill Salt'), ('Fill Pepper'), ('Dust Lights'), ('Wipe Expo Line');
 
 INSERT INTO list(list_name)
 VALUES('test list');
